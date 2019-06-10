22db947981ad0b7f409acc1a950d3b6affe06be7
amyklopfen <50849294+amyklopfen@users.noreply.github.com>
2019-06-09 20:50:42 -0400

    Update groceries.py

--- groceries.py
+++ groceries.py
@@ -25,7 +25,16 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-print(products)
+
+print("----------------")
+print("There are 20 products:")
+print("----------------")
+
+for i in products:
+    print("+", i["name"], "($",i["price"],")")
+
+#based on solution found on Stack Overflow:https://stackoverflow.com/questions/33661893/typeerror-unhashable-type-dict-dictionaries-within-lists
+
+
 # pprint(products)
 
-# TODO: write some Python code here to produce the desired output
